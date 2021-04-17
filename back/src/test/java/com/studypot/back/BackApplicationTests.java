d322929eefdffa35d68446fa35c95615f1e26154
sungsan <kunga077@gmail.com>
2021-04-17 20:26:36 +0900

    back-end 초기세팅

---
+++ back/src/test/java/com/studypot/back/BackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.studypot.back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
