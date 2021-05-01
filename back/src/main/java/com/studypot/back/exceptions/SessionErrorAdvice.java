5f2f96693a840dbce3aa4860ad3c632ed0e030f0
sungsan <kunga077@gmail.com>
2021-05-02 00:00:25 +0900

    리프레시 토큰 생성과 확인 구현
    예외처리 핸들링 추가

--- back/src/main/java/com/studypot/back/exceptions/SessionErrorAdvice.java
+++ back/src/main/java/com/studypot/back/exceptions/SessionErrorAdvice.java
@@ -1,7 +1,6 @@
 package com.studypot.back.exceptions;
 
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
--- back/src/main/java/com/studypot/back/utils/JwtUtil.java
+++ back/src/main/java/com/studypot/back/utils/JwtUtil.java
@@ -1,14 +1,15 @@
 package com.studypot.back.utils;
 
 import io.jsonwebtoken.Claims;
-import io.jsonwebtoken.Jws;
 import io.jsonwebtoken.JwtBuilder;
 import io.jsonwebtoken.JwtParser;
 import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.MalformedJwtException;
 import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.security.Keys;
 import io.jsonwebtoken.security.SignatureException;
 import java.security.Key;
+import java.util.Date;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
@@ -17,14 +18,41 @@ public class JwtUtil {
 
   private final Key key;
 
+  private int jwtExpirationTimeMs;
+  private int refreshExpirationTimeMs;
+
   public JwtUtil(@Value("${jwt.secret}") String secretKey) {
     this.key = Keys.hmacShaKeyFor(secretKey.getBytes());
+  }
+
+  @Value("${jwt.jwtExpirationTimeMs}")
+  public void setJwtExpirationTimeMs(int jwtExpirationTimeMs) {
+    this.jwtExpirationTimeMs = jwtExpirationTimeMs;
+  }
+
+  @Value("${jwt.refreshExpirationTimeMs}")
+  public void setRefreshExpirationTimeMs(int refreshExpirationTimeMs) {
+    this.refreshExpirationTimeMs = refreshExpirationTimeMs;
   }
 
   public String createToken(Long userId, String userName) {
     JwtBuilder builder = Jwts.builder()
         .claim("userId", userId)
-        .claim("userName", userName);
+        .claim("userName", userName)
+        .setIssuedAt(new Date(System.currentTimeMillis()))
+        .setExpiration(new Date(System.currentTimeMillis() + jwtExpirationTimeMs));
+
+    return builder
+        .signWith(key, SignatureAlgorithm.HS256)
+        .compact();
+  }
+
+  public String createRefreshToken(Long userId, String userName) {
+    JwtBuilder builder = Jwts.builder()
+        .claim("userId", userId)
+        .claim("userName", userName)
+        .setIssuedAt(new Date(System.currentTimeMillis()))
+        .setExpiration(new Date(System.currentTimeMillis() + refreshExpirationTimeMs));
 
     return builder
         .signWith(key, SignatureAlgorithm.HS256)
@@ -39,8 +67,9 @@ public class JwtUtil {
     try {
       return parser.parseClaimsJws(token).getBody();
     } catch (SignatureException e) {
-
-      return null;
+      throw new SignatureException("Bad Signature", e);
+    } catch (MalformedJwtException e) {
+      throw new MalformedJwtException("This token was not correctly constructed", e);
     }
   }
 }
