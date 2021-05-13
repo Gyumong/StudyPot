75a490d44c27b736390b960e04d188a575bb35af
sungsan <kunga077@gmail.com>
2021-05-13 21:07:00 +0900

    :card_file_box: AWS S3 연결
    AWS S3 연결하였습니다. S3Service와 관련해서는 interface로 패턴을 적용해보았습니다.

--- back/src/main/java/com/studypot/back/interfaces/ProfileController.java
+++ back/src/main/java/com/studypot/back/interfaces/ProfileController.java
@@ -1,6 +1,6 @@
 package com.studypot.back.interfaces;
 
-import com.studypot.back.applications.UserService;
+import com.studypot.back.applications.ProfileService;
 import com.studypot.back.auth.UserId;
 import com.studypot.back.auth.UserName;
 import com.studypot.back.dto.user.ProfileProjection;
@@ -8,36 +8,35 @@ import com.studypot.back.dto.user.ProfileResponseDto;
 import com.studypot.back.dto.user.UpdateProfileRequestDto;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
+import java.io.IOException;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @Api("프로필")
+@RequiredArgsConstructor
+@Slf4j
 public class ProfileController {
 
-  private final UserService userService;
-
-  public ProfileController(UserService userService) {
-    this.userService = userService;
-  }
-
+  private final ProfileService profileService;
 
   @GetMapping("/user")
   @ApiOperation("유저 정보 조회")
   public ProfileProjection getUser(@UserName String userName) {
 
-    return userService.getProfile(userName);
+    return profileService.getProfile(userName);
   }
 
   @PatchMapping("/user")
   @ApiOperation("유저 정보 수정")
   public ProfileResponseDto updateUser(
       @UserId Long userId,
-      @RequestBody UpdateProfileRequestDto updateProfileRequestDto) {
+      UpdateProfileRequestDto updateProfileRequestDto) throws IOException {
 
-    return userService.updateProfile(userId, updateProfileRequestDto);
+    return profileService.updateProfile(userId, updateProfileRequestDto);
   }
 
 }
