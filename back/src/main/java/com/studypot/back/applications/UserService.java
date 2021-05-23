9ef4bcdfba3935cbc30067a28826dc1f606da3a3
sungsan <kunga077@gmail.com>
2021-05-23 18:24:17 +0900

    :zap: STUDYPOT-19 Profile 업데이트 로직 수정
    AWS S3에 이미지 파일을 저장하는 로직을 추가

--- back/src/main/java/com/studypot/back/applications/UserService.java
+++ back/src/main/java/com/studypot/back/applications/UserService.java
@@ -2,12 +2,8 @@ package com.studypot.back.applications;
 
 import com.studypot.back.domain.User;
 import com.studypot.back.domain.UserRepository;
-import com.studypot.back.dto.user.ProfileProjection;
-import com.studypot.back.dto.user.ProfileResponseDto;
-import com.studypot.back.dto.user.UpdateProfileRequestDto;
 import com.studypot.back.exceptions.ExistEmailException;
 import com.studypot.back.exceptions.UnregisteredEmailException;
-import com.studypot.back.exceptions.UserNotFoundException;
 import com.studypot.back.exceptions.WrongPasswordException;
 import java.util.Optional;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -51,18 +47,6 @@ public class UserService {
     }
 
     return user;
-  }
-
-  public ProfileProjection getProfile(String name) {
-    return userRepository.findByName(name).orElseThrow(UserNotFoundException::new);
-  }
-
-  public ProfileResponseDto updateProfile(Long userId, UpdateProfileRequestDto updateProfileRequestDto) {
-    User updateUser = userRepository.findById(userId).orElseThrow(UserNotFoundException::new);
-    ProfileResponseDto profileResponseDto = updateUser.updateProfile(updateProfileRequestDto);
-    userRepository.save(updateUser);
-
-    return profileResponseDto;
   }
 
   public User checkRefreshToken(Long userId) {
