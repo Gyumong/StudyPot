74af2047070fa9905504ab52c7e222a5fdf4c860
sungsan <kunga077@gmail.com>
2021-05-25 16:52:21 +0900

    :memo: STUDYPOT-3 PR 수정 요구사항에 대한 문서 변경 사항
    불필요한 TODO 제거 및 TODO 업데이트

--- back/src/main/java/com/studypot/back/domain/CategoryRepository.java
+++ back/src/main/java/com/studypot/back/domain/CategoryRepository.java
@@ -5,6 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CategoryRepository extends JpaRepository<Category, Long> {
 
+  //TODO: Transactional 분석
   @Transactional
   void deleteAllByUserId(Long userId);
 }
