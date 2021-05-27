3e9098b9dd9b3a7cee0db30d9dd6ce2567776dad
Gyumong <rlaalsrb3559@naver.com>
2021-05-27 21:13:56 +0900

    Add: refreshToken 로직

--- front/hooks/useMyInfo.ts
+++ front/hooks/useMyInfo.ts
@@ -5,6 +5,10 @@ type ReturnTypes<T = any> = [T, () => void];
 type AccessToken = {
   accessToken: string;
 };
+type RefershToken = {
+  refreshToken: string;
+};
+
 const useMyInfo = (): ReturnTypes => {
   const fetcher = async (url: string) => {
     if (localStorage.getItem("accessToken")) {
@@ -18,9 +22,7 @@ const useMyInfo = (): ReturnTypes => {
       } catch (e) {
         const { accessToken }: AccessToken = await axios.get("/refresh", {
           headers: {
-            headers: {
-              Authorization: `Bearer ${localStorage.getItem("accessToken")}`,
-            },
+            Authorization: `Bearer ${localStorage.getItem("refreshToken")}`,
           },
         });
         localStorage.setItem("accessToken", accessToken);
