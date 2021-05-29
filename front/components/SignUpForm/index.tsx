c7b3749abfa4362f7a7ecb0a888b831ead2fd1df
FromGoodEnoughYounGyeom <webdjanelee@gmail.com>
2021-05-29 14:40:49 +0900

    add: 회원가입페이지에 관심사 select bar 추가

--- front/components/SignUpForm/index.tsx
+++ front/components/SignUpForm/index.tsx
@@ -1,8 +1,27 @@
 import React, { useState, ReactElement, useCallback, useEffect } from "react";
-import { Header, SignUpFormBlock, Input, SignUpButton, Desc, Error, Success } from "./styles";
+import { Header, SignUpFormBlock, Input, SignUpButton, Desc, Error, Success, Interest, SelfIntro, colourStyles, SignUpInnerBox, SelectBox } from "./styles";
 import useInput from "@hooks/useInput";
 import Link from "next/link";
 import axios from "axios";
+import Select, { ActionMeta, ValueType } from "react-select";
+import useMyInfo from "@hooks/useMyInfo";
+
+type IOptionType = { label: string; value: number; color?: string; isFixed?: boolean; isDisabled?: boolean };
+type IsMulti = true | false;
+
+const Option: IOptionType[] = [
+  { value: 1, label: "IT", color: "#00B8D9", isFixed: true },
+  { value: 2, label: "Front", color: "#0052CC", isDisabled: true },
+  { value: 3, label: "React", color: "#5243AA" },
+  { value: 4, label: "Java", color: "#FF5630", isFixed: true },
+  { value: 5, label: "IOS", color: "#FF8B00" },
+  { value: 6, label: "NodeJS", color: "#FFC400" },
+  { value: 7, label: "NextJS", color: "#36B37E" },
+  { value: 8, label: "A", color: "#00875A" },
+  { value: 9, label: "B", color: "#253858" },
+  { value: 10, label: "C", color: "#666666" },
+];
+
 
 const SignUpForm = (): ReactElement => {
   const [email, onChangeEmail] = useInput("");
@@ -65,22 +84,45 @@ const SignUpForm = (): ReactElement => {
     [email, name, password, passwordCheck, mismatchError],
   );
 
+  const [userData] = useMyInfo();
+  const [FavoriteValue, setFavoriteValue] = useState([] as IOptionType[]);
+  const onChangeFavorite = useCallback(
+    (value: ValueType<IOptionType, IsMulti>, _: ActionMeta<IOptionType>) => {
+      setFavoriteValue(value as IOptionType[]);
+    },
+    [FavoriteValue],
+  );
+
   return (
     <SignUpFormBlock onSubmit={onSubmit}>
+      
       <Header>회원가입</Header>
-      <Input type="text" placeholder="이름" value={name} onChange={onChangeName} />
-      <Input type="email" placeholder="이메일" value={email} onChange={onChangeEmail} />
-      <Input
-        type="password"
-        placeholder="비밀번호(문자,숫자조합 8자 이상)"
-        value={password}
-        onChange={onChangePassword}
-      />
-      <Input type="password" placeholder="비밀번호 체크" value={passwordCheck} onChange={onChangePasswordCheck} />
-      {passwordError && <Error>비밀번호는 문자,숫자조합 8자 이상만 가능합니다.</Error>}
-      {mismatchError && <Error>비밀번호가 일치하지 않습니다.</Error>}
-      {signUpError && <Error>{signUpError}</Error>}
-      {signUpSuccess && <Success>회원가입되었습니다! 로그인해주세요.</Success>}
+      
+      <SignUpInnerBox>
+        <Input type="text" placeholder="이름" value={name} onChange={onChangeName} />
+        <Input type="email" placeholder="이메일" value={email} onChange={onChangeEmail} />
+        <Input
+          type="password"
+          placeholder="비밀번호(문자,숫자조합 8자 이상)"
+          value={password}
+          onChange={onChangePassword}
+        />
+        <Input type="password" placeholder="비밀번호 체크" value={passwordCheck} onChange={onChangePasswordCheck} />
+        {passwordError && <Error>비밀번호는 문자,숫자조합 8자 이상만 가능합니다.</Error>}
+        {mismatchError && <Error>비밀번호가 일치하지 않습니다.</Error>}
+        {signUpError && <Error>{signUpError}</Error>}
+        {signUpSuccess && <Success>회원가입되었습니다! 로그인해주세요.</Success>}
+        
+        <SelectBox>
+        <Select isMulti value={FavoriteValue} options={Option} onChange={onChangeFavorite} styles={colourStyles} placeholder="관심사" />
+        <SelfIntro>{userData?.introduction}</SelfIntro>
+        </SelectBox>
+        
+      </SignUpInnerBox>
+
+
+      
+      
       <Desc>
         계정 만들기 버튼을 클릭하면, <strong>스터디팟</strong> 의 <u>회원약관</u>에 동의하며 <br />
         쿠키 사용을 포함한 <u>개인정보처리방침</u>을 읽었음을 인정하게 됩니다.
--- front/components/SignUpForm/styles.tsx
+++ front/components/SignUpForm/styles.tsx
@@ -1,6 +1,45 @@
 import styled from "@emotion/styled";
 import Button from "@components/common/Button";
 import palette from "@styles/palette";
+import chroma from "chroma-js";
+
+
+export const colourStyles = {
+  control: (styles) => ({ ...styles, backgroundColor: "white" }),
+  option: (styles, { data, isDisabled, isFocused, isSelected }) => {
+    const color = chroma(data.color);
+    return {
+      ...styles,
+      backgroundColor: isDisabled ? null : isSelected ? data.color : isFocused ? color.alpha(0.1).css() : null,
+      color: isDisabled ? "#ccc" : isSelected ? (chroma.contrast(color, "white") > 2 ? "white" : "black") : data.color,
+      cursor: isDisabled ? "not-allowed" : "default",
+
+      ":active": {
+        ...styles[":active"],
+        backgroundColor: !isDisabled && (isSelected ? data.color : color.alpha(0.3).css()),
+      },
+    };
+  },
+  multiValue: (styles, { data }) => {
+    const color = chroma(data.color);
+    return {
+      ...styles,
+      backgroundColor: color.alpha(0.1).css(),
+    };
+  },
+  multiValueLabel: (styles, { data }) => ({
+    ...styles,
+    color: data.color,
+  }),
+  multiValueRemove: (styles, { data }) => ({
+    ...styles,
+    color: data.color,
+    ":hover": {
+      backgroundColor: data.color,
+      color: "white",
+    },
+  }),
+};
 
 export const SignUpFormBlock = styled.form`
   width: 80vw;
@@ -70,4 +109,28 @@ export const Success = styled.div`
 `;
 export const SignUpButton = styled(Button)`
   margin-top: 2rem;
+`;
+
+
+export const SelfIntro = styled.p`
+  color: #4f4f4f;
+`;
+
+export const SignUpInnerBox = styled.p`
+  
+`;
+
+export const Interest = styled.div`
+  margin-bottom: 3rem;
+  & > p {
+    font-size: 1rem;
+    color: #bdbdbd;
+    margin-top: 0;
+  }
+`;
+
+export const SelectBox = styled.div`
+  padding-top : 2rem;
+  font-size: 1rem;
+  
 `;
