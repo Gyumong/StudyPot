ccb2a067771cc42c28ee88762c496b4c42b4f9aa
Jane <webdjanelee@gmail.com>
2021-06-14 20:29:21 +0900

    🎨 스터디찾기페이지 gridBox 정렬추가

--- front/components/FindStudy/index.tsx
+++ front/components/FindStudy/index.tsx
@@ -1,13 +1,20 @@
 import {AuthTemplateBlock} from "./styles";
 import StudyCard from "@components/StudyCard"
+import {GridBox} from "./styles"
 
 const FindStudy = () => {
  
     return (
        <AuthTemplateBlock>
-           <StudyCard/>
-           <StudyCard/>
-           <StudyCard/>
+           <GridBox>
+                <StudyCard/>
+                <StudyCard/>
+                <StudyCard/>
+                <StudyCard/>
+                <StudyCard/>
+                <StudyCard/>
+                <StudyCard/>
+           </GridBox>
        </AuthTemplateBlock>
     )
 };
