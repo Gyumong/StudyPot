9ff4eaaab89a4eee96a332e1deb9a91e976cee00
leokim <kunga077@gmail.com>
2021-06-25 16:56:54 +0900

    :wrench: STUDYPOT-67 Http 메소드 허용 수정
    *로 적용이 안되던 항목을 스트링 리스트로 수정

--- back/src/main/java/com/studypot/back/WebConfigurerImpl.java
+++ back/src/main/java/com/studypot/back/WebConfigurerImpl.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.boot.autoconfigure.web.ErrorProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -30,7 +31,15 @@ public class WebConfigurerImpl extends WebSecurityConfigurerAdapter implements W
   public void addCorsMappings(CorsRegistry registry) {
     registry.addMapping("/**")
         .allowedOrigins("*")
-        .allowedMethods("*");
+        .allowedMethods(
+            HttpMethod.GET.name(),
+            HttpMethod.POST.name(),
+            HttpMethod.PUT.name(),
+            HttpMethod.PATCH.name(),
+            HttpMethod.DELETE.name(),
+            HttpMethod.HEAD.name(),
+            HttpMethod.GET.name()
+        );
 
   }
 
