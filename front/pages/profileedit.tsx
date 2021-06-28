362aee8ebb8f891ac55f9e84101a3dd2e3f60e9f
Gyumong <rlaalsrb3559@naver.com>
2021-06-28 15:08:14 +0900

    🩹 모듈 안불러온 부분 수정

--- front/pages/profileedit.tsx
+++ front/pages/profileedit.tsx
@@ -1,8 +1,9 @@
-import React, { ReactElement } from "react";
+import React, { ReactElement, useEffect } from "react";
 import AuthTemplate from "@layouts/auth";
 import Header from "@components/Header";
 import ProfileEditForm from "@components/ProfileEditForm";
 import { useDispatch, useSelector } from "react-redux";
+import { loadUserByToken } from "@lib/slices/UserSlice";
 const ProfileEdit = (): ReactElement => {
   const dispatch = useDispatch();
   useEffect(() => {
