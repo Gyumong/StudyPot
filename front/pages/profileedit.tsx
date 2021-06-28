3f15247910e236bd2c20857bef54c527998eb089
Gyumong <rlaalsrb3559@naver.com>
2021-06-28 15:06:12 +0900

    🎨 마이페이지수정, 스터디모집 페이지에서 유저정보 가져오기

--- front/pages/profileedit.tsx
+++ front/pages/profileedit.tsx
@@ -1,14 +1,18 @@
 import React, { ReactElement } from "react";
 import AuthTemplate from "@layouts/auth";
 import Header from "@components/Header";
-import ProfileEditForm from "@components/ProfileEditForm"
-
+import ProfileEditForm from "@components/ProfileEditForm";
+import { useDispatch, useSelector } from "react-redux";
 const ProfileEdit = (): ReactElement => {
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(loadUserByToken(null));
+  }, []);
   return (
     <>
       <Header />
       <AuthTemplate>
-        <ProfileEditForm/>
+        <ProfileEditForm />
       </AuthTemplate>
     </>
   );
--- front/pages/recruit.tsx
+++ front/pages/recruit.tsx
@@ -1,13 +1,16 @@
 import React, { ReactElement } from "react";
 import AuthTemplate from "@layouts/auth";
-import RecruitForm from "@components/RecruitForm"
-
-
+import RecruitForm from "@components/RecruitForm";
+import { useDispatch, useSelector } from "react-redux";
 
 const Recruit = (): ReactElement => {
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(loadUserByToken(null));
+  }, [dispatch]);
   return (
     <AuthTemplate>
-      <RecruitForm/>
+      <RecruitForm />
     </AuthTemplate>
   );
 };
