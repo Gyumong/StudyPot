c16beac42f833b0bc647ed7003705ffceed72f0e
bum752 <bum752@gmail.com>
2021-08-08 17:22:23 +0900

    ✨ STUDYPOT-137 마이페이지에 내 스터디, 관심 스터디 목록 노출

--- front/components/MyPageForm/index.tsx
+++ front/components/MyPageForm/index.tsx
@@ -1,8 +1,12 @@
 import React, { ReactElement } from "react";
 import { MypageFormBlock, StudyList, StudyListBox, StudyName, StudyDetail } from "./styles";
 import Profile from "@components/Profile";
+import { useSelector } from "react-redux";
+import { RootState } from "@lib/slices";
 
 const MyPageForm = (): ReactElement => {
+  const { user } = useSelector((state: RootState) => state.users);
+
   return (
     <MypageFormBlock>
       <Profile />
@@ -11,19 +15,31 @@ const MyPageForm = (): ReactElement => {
         <p>내스터디</p>
         <button>수정</button>
       </StudyList>
-      <StudyListBox>
-        <StudyName>디스코드 캠스터디</StudyName>
-        <StudyDetail>디스코드로 캠 켜고 같이 공부해요! 얼굴은 안나오고 손만 켤...</StudyDetail>
-      </StudyListBox>
+
+      {user.participatingStudyList &&
+        user.participatingStudyList.map((study: any) => {
+          return (
+            <StudyListBox key={study.studyId}>
+              <StudyName>{study.studyTitle}</StudyName>
+              <StudyDetail>{study.studyContent}</StudyDetail>
+            </StudyListBox>
+          );
+        })}
 
       <StudyList>
         <p>관심 스터디</p>
         <button>수정</button>
       </StudyList>
-      <StudyListBox>
-        <StudyName>디스코드 캠스터디</StudyName>
-        <StudyDetail>디스코드로 캠 켜고 같이 공부해요! 얼굴은 안나오고 손만 켤...</StudyDetail>
-      </StudyListBox>
+
+      {user.interestingStudyList &&
+        user.interestingStudyList.map((study: any) => {
+          return (
+            <StudyListBox key={study.studyId}>
+              <StudyName>{study.studyTitle}</StudyName>
+              <StudyDetail>{study.studyContent}</StudyDetail>
+            </StudyListBox>
+          );
+        })}
     </MypageFormBlock>
   );
 };
